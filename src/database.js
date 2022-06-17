ab830aa8041ee183b3ace0f6d6292cadafbade51
Juan Fernando Zuluaga <hackingetico2021@gmail.com>
2022-06-17 14:44:40 -0500

    Updated 17-06-2022, build and mode production

--- src/database.js
+++ src/database.js
@@ -1,10 +1,11 @@
 const mongoose =require('mongoose');
 
-const URI = 'mongodb://localhost/project';
+// const URI = 'mongodb://localhost/project';
 
-mongoose.connect(URI)
-    .then(db => console.log('DB is connected'))
-    .catch(err => console.error(err));
+// mongoose.connect(URI)
+//     .then(db => console.log('DB is connected'))
+//     .catch(err => console.error(err));
 
-
+mongoose.connect("mongodb+srv://JuanFernandoZuluaga:tSChsAq0uhxAJej1@appreactv1.vpfncrd.mongodb.net/AppReactV1")
+    .then(db => console.log('DB is conected'))
 module.exports = mongoose;
